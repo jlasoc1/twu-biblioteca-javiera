08aa857d56595605dca4a92b220e09bf14ac2c91
Javiera Laso <javiera@MacBook-Pro-de-Javiera.local>
2019-05-28 22:33:15 -0400

    Creation of the greeting and the booklist (internal answer)

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,53 @@
 package com.twu.biblioteca;
 
-public class BibliotecaApp {
+import java.util.List;
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+public class BibliotecaApp
+{
+
+    private List<Book> bookList;
+
+    public static void main(String[] args)
+    {
+
     }
+
+    public String getAnswer(String _sUserInput)
+    {
+        return getGreeting() + getMenu();
+    }
+
+
+    public void setBooks(List bookList)
+    {
+        this.bookList = bookList;
+
+    }
+
+    private String getGreeting()
+    {
+        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore\n";
+    }
+
+    private String getMenu()
+    {
+        return "This is the menu, you can select: \n '1. List of Books'";
+    }
+
+
+    public String getBookList()
+    {
+        String resultList = "{";
+        for (Book b : bookList)
+        {
+            resultList = resultList + b.getAuthorOfTheBook() + " : " + b.getNameOfTheBook() + " : "
+                    + b.getYearOfPublishing() + " " + "\n" ;
+
+        }
+        resultList = resultList + "}";
+
+        return resultList;
+    }
+
+
 }
