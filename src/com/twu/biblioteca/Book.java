20c826ab2fdcf6b685972187e04b3b21b94c273f
Javiera Laso <javiera@MacBook-Pro-de-Javiera.local>
2019-05-28 22:34:56 -0400

    Creation of Book java class that contains the attributes of the booklist

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,4 +1,40 @@
 package com.twu.biblioteca;
 
-public class Book {
+
+public class Book
+{
+    private String authorOfTheBook;
+    private String nameOfTheBook;
+    private int yearOfPublishing;
+
+
+
+    public Book(String authorOfTheBook, String nameOfTheBook, int yearOfPublishing)
+    {
+        this.authorOfTheBook = authorOfTheBook;
+        this.nameOfTheBook = nameOfTheBook;
+        this.yearOfPublishing = yearOfPublishing;
+
+
+    }
+
+
+// getters
+
+    public String getAuthorOfTheBook()
+    {
+        return authorOfTheBook;
+    }
+
+    public String getNameOfTheBook()
+    {
+        return nameOfTheBook;
+    }
+
+    public int getYearOfPublishing()
+    {
+        return yearOfPublishing;
+    }
 }
+
+
